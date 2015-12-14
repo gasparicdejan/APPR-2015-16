ac18fac93968968c2be9378b01e2211d0991a3a8
Dejan <deyangasparic@gmail.com>
2015-12-14 21:05:43 +0100

    Dodajanje tabel

--- uvoz/README.md
+++ uvoz/README.md
@@ -1,4 +1,1 @@
 # Obdelava, uvoz in čiščenje podatkov.
-
-Tukaj bomo imeli program, ki bo obdelal, uvozil in očistil podatke (druga faza
-projekta).
--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -18,3 +18,50 @@
 # # datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
 # # 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
 # # fazah.
+
+uvozi1 <- function() {return(read.delim("podatki/animals.csv", sep = ";", as.is = TRUE,
+                                         row.names = NULL, na.strings= c("Partner Name", "2010", "2011", "2012", "2013", "2014"),
+                                         fileEncoding = "Windows-1250"))}
+
+cat("Uvažam podatke o uvoz-u živali (tabela 1)\n")
+zivali <- uvozi1()
+
+
+
+
+uvozi2 <- function() {return(read.delim("podatki/chemical.csv", sep = ";", as.is = TRUE,
+                                        row.names = NULL, na.strings= c("Partner Name", "2010", "2011", "2012", "2013", "2014"),
+                                        fileEncoding = "Windows-1250"))}
+
+cat("Uvažam podatke o uvoz-u kemikalij (tabela 1)\n")
+kemikalije <- uvozi2()
+
+
+
+
+uvozi3 <- function() {return(read.delim("podatki/fuels.csv", sep = ";", as.is = TRUE,
+                                        row.names = NULL, na.strings= c("Partner Name", "2010", "2011", "2012", "2013", "2014"),
+                                        fileEncoding = "Windows-1250"))}
+
+cat("Uvažam podatke o uvoz-u goriv (tabela 1)\n")
+goriva <- uvozi3()
+
+
+
+
+uvozi4 <- function() {return(read.delim("podatki/minerals.csv", sep = ";", as.is = TRUE,
+                                        row.names = NULL, na.strings= c("Partner Name", "2010", "2011", "2012", "2013", "2014"),
+                                        fileEncoding = "Windows-1250"))}
+
+cat("Uvažam podatke o uvoz-u mineralov (tabela 1)\n")
+minerali <- uvozi4()
+
+
+
+
+uvozi5 <- function() {return(read.delim("podatki/metals.csv", sep = ";", as.is = TRUE,
+                                        row.names = NULL, na.strings= c("Partner Name", "2010", "2011", "2012", "2013", "2014"),
+                                        fileEncoding = "Windows-1250"))}
+
+cat("Uvažam podatke o uvoz-u kovin (tabela 1)\n")
+kovine <- uvozi5()
