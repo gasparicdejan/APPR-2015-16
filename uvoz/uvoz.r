b6177ff9535179e2f5f5766d4c15c66035e63510
Dejan <deyangasparic@gmail.com>
2015-12-22 23:44:47 +0100

    Dodal dva grafa ki še ne delujeta, moram odpraviti napako

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,9 +1,10 @@
 # 2. faza: Uvoz podatkov
 
-# Uvoz ZDA:
+# PODATKI V OBLIKI csv :
+# Uvoz ZDA :
 
 tabela_uvoz_vseh_produktov <- read.csv2("podatki/import_all_product.csv", skip=1, na.strings = ";", stringsAsFactors = FALSE,
-                             fileEncoding = "Windows-1250", col.names = c("Država","2010", "2011", "2012", "2013", "2014"))
+                             fileEncoding = "Windows-1250", col.names = c("Država", "2010", "2011", "2012", "2013", "2014"))
 
 tabela_uvoz_zivali <- read.csv2("podatki/import_animal.csv", skip=1, na.strings = ";", stringsAsFactors = FALSE,
                                         fileEncoding = "Windows-1250", col.names = c("Država","2010", "2011", "2012", "2013", "2014"))
@@ -25,6 +26,7 @@ tabela_uvoz_plastike_in_gume <- read.csv2("podatki/import_plastic_and_rubber.csv
 
 tabela_uvoz_lesa <- read.csv2("podatki/import_wood.csv", skip=1, na.strings = ";", stringsAsFactors = FALSE,
                                         fileEncoding = "Windows-1250", col.names = c("Država","2010", "2011", "2012", "2013", "2014"))
+
 
 # Izvoz ZDA:
 
@@ -49,5 +51,51 @@ tabela_izvoz_kemikalij <- read.csv2("podatki/export_chemicals.csv", skip=1, na.s
 tabela_izvoz_plastike_in_gume <- read.csv2("podatki/export_plastic_and_rubber.csv", skip=1, na.strings = ";", stringsAsFactors = FALSE,
                                           fileEncoding = "Windows-1250", col.names = c("Država","2010", "2011", "2012", "2013", "2014"))
 
-tabela_uvoz_lesa <- read.csv2("podatki/import_wood.csv", skip=1, na.strings = ";", stringsAsFactors = FALSE,
+tabela_izvoz_lesa <- read.csv2("podatki/export_wood.csv", skip=1, na.strings = ";", stringsAsFactors = FALSE,
                               fileEncoding = "Windows-1250", col.names = c("Država","2010", "2011", "2012", "2013", "2014"))
+
+
+
+# PODATKI V OBLIKI xml :
+# uvoz tabele držav, za katere je ZDA glavni partner v izvozu v % :
+# uvoz tabele držav, za katere je ZDA glavni partner v uvozu v % :
+
+
+
+# Grafi :
+# celoten uvoz ZDA
+
+Države<-tabela_uvoz_vseh_produktov$Država
+uvoz_v_tisoč_USD_v_letu_2010<-tabela_uvoz_vseh_produktov$X2010
+uvoz_v_tisoč_USD_v_letu_2011<-tabela_uvoz_vseh_produktov$X2011
+uvoz_v_tisoč_USD_v_letu_2012<-tabela_uvoz_vseh_produktov$X2012
+uvoz_v_tisoč_USD_v_letu_2013<-tabela_uvoz_vseh_produktov$X2013
+uvoz_v_tisoč_USD_v_letu_2014<-tabela_uvoz_vseh_produktov$X2014
+
+uvoz_v_celoti <- ggplot(tabela_uvoz_vseh_produktov)+ 
+    aes(x=Države, y=c("uvoz_v_tisoč_USD_v_letu_2010","uvoz_v_tisoč_USD_v_letu_2011",
+    "uvoz_v_tisoč_USD_v_letu_2012","uvoz_v_tisoč_USD_v_letu_2013","uvoz_v_tisoč_USD_v_letu_2014"))+
+    geom_line(colour = "red")+
+    ggtitle("Skupni uvoz ZDA v letih 2010 do 2014")+
+    theme(plot.title = element_text(lineheight=.8, face="bold"))
+
+uvoz_v_celoti
+
+
+# celoten izvoz ZDA
+
+Države<-tabela_izvoz_vseh_produktov$Država
+izvoz_v_tisoč_USD_v_letu_2010<-tabela_izvoz_vseh_produktov$X2010
+izvoz_v_tisoč_USD_v_letu_2011<-tabela_izvoz_vseh_produktov$X2011
+izvoz_v_tisoč_USD_v_letu_2012<-tabela_izvoz_vseh_produktov$X2012
+izvoz_v_tisoč_USD_v_letu_2013<-tabela_izvoz_vseh_produktov$X2013
+izvoz_v_tisoč_USD_v_letu_2014<-tabela_izvoz_vseh_produktov$X2014
+
+izvoz_v_celoti<-ggplot(tabela_izvoz_vseh_produktov)+
+  aes(x=Države, y=c("izvoz_v_tisoč_USD_v_letu_2010","izvoz_v_tisoč_USD_v_letu_2011",
+                    "izvoz_v_tisoč_USD_v_letu_2012","izvoz_v_tisoč_USD_v_letu_2013","izvoz_v_tisoč_USD_v_letu_2014"))+
+  geom_line(colour = "red")+
+  ggtitle("Skupni izvoz ZDA v letih 2010 do 2014")+
+  theme(plot.title = element_text(lineheight=.8, face="bold"))
+
+izvoz_v_celoti
