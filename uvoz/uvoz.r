369c8b3f66ac3704ce8810a6f227f6592d23165c
Dejan Gašparič <deyangasparic@gmail.com>
2016-01-15 12:08:23 +0100

    uredil tabele... še je potrebno malce popraviti

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -5,55 +5,149 @@
 
 tabela_uvoz_vseh_produktov <- read.csv2("podatki/import_all_product.csv", skip=1, na.strings = ";", stringsAsFactors = FALSE,
                              fileEncoding = "Windows-1250", col.names = c("Država", "2010", "2011", "2012", "2013", "2014"))
+tabela_uvoz_vseh_produktov <- melt(tabela_uvoz_vseh_produktov, variable.name = "Leto",
+                            value.name = "Vsi produkti", na.rm = TRUE)
+tabela_uvoz_vseh_produktov$Leto <- tabela_uvoz_vseh_produktov$Leto %>% as.character() %>%
+  strapplyc("([0-9]+)")
+tabela_uvoz_vseh_produktov$Leto <- tabela_uvoz_vseh_produktov$Leto %>% as.numeric()
+
 
 tabela_uvoz_zivali <- read.csv2("podatki/import_animal.csv", skip=1, na.strings = ";", stringsAsFactors = FALSE,
                                         fileEncoding = "Windows-1250", col.names = c("Država","2010", "2011", "2012", "2013", "2014"))
+tabela_uvoz_zivali <- melt(tabela_uvoz_zivali, variable.name = "Leto",
+                            value.name = "Živali", na.rm = TRUE)
+tabela_uvoz_zivali$Leto <- tabela_uvoz_zivali$Leto %>% as.character() %>%
+  strapplyc("([0-9]+)")
+tabela_uvoz_zivali$Leto <- tabela_uvoz_zivali$Leto %>% as.numeric()
+
 
 tabela_uvoz_zelenjave <- read.csv2("podatki/import_vegetable.csv", skip=1, na.strings = ";", stringsAsFactors = FALSE,
                                         fileEncoding = "Windows-1250", col.names = c("Država","2010", "2011", "2012", "2013", "2014"))
+tabela_uvoz_zelenjave <- melt(tabela_uvoz_zelenjave, variable.name = "Leto",
+                            value.name = "Zelenjava", na.rm = TRUE)
+tabela_uvoz_zelenjave$Leto <- tabela_uvoz_zelenjave$Leto %>% as.character() %>%
+  strapplyc("([0-9]+)")
+tabela_uvoz_zelenjave$Leto <- tabela_uvoz_zelenjave$Leto %>% as.numeric()
+
 
 tabela_uvoz_mineralov <- read.csv2("podatki/import_minerals.csv", skip=1, na.strings = ";", stringsAsFactors = FALSE,
                                         fileEncoding = "Windows-1250", col.names = c("Država","2010", "2011", "2012", "2013", "2014"))
+tabela_uvoz_mineralov <- melt(tabela_uvoz_mineralov, variable.name = "Leto",
+                            value.name = "Minerali", na.rm = TRUE)
+tabela_uvoz_mineralov$Leto <- tabela_uvoz_mineralov$Leto %>% as.character() %>%
+  strapplyc("([0-9]+)")
+tabela_uvoz_mineralov$Leto <- tabela_uvoz_mineralov$Leto %>% as.numeric()
+
 
 tabela_uvoz_goriva <- read.csv2("podatki/import_fuels.csv", skip=1, na.strings = ";", stringsAsFactors = FALSE,
                                         fileEncoding = "Windows-1250", col.names = c("Država","2010", "2011", "2012", "2013", "2014"))
+tabela_uvoz_goriva <- melt(tabela_uvoz_goriva, variable.name = "Leto",
+                            value.name = "Gorivo", na.rm = TRUE)
+tabela_uvoz_goriva$Leto <- tabela_uvoz_goriva$Leto %>% as.character() %>%
+  strapplyc("([0-9]+)")
+tabela_uvoz_goriva$Leto <- tabela_uvoz_goriva$Leto %>% as.numeric()
+
 
 tabela_uvoz_kemikalij <- read.csv2("podatki/import_chemicals.csv", skip=1, na.strings = ";", stringsAsFactors = FALSE,
                                         fileEncoding = "Windows-1250", col.names = c("Država","2010", "2011", "2012", "2013", "2014"))
+tabela_uvoz_kemikalij <- melt(tabela_uvoz_kemikalij, variable.name = "Leto",
+                            value.name = "Kemikalije", na.rm = TRUE)
+tabela_uvoz_kemikalij$Leto <- tabela_uvoz_kemikalij$Leto %>% as.character() %>%
+  strapplyc("([0-9]+)")
+tabela_uvoz_kemikalij$Leto <- tabela_uvoz_kemikalij$Leto %>% as.numeric()
+
 
 tabela_uvoz_plastike_in_gume <- read.csv2("podatki/import_plastic_and_rubber.csv", skip=1, na.strings = ";", stringsAsFactors = FALSE,
                                         fileEncoding = "Windows-1250", col.names = c("Država","2010", "2011", "2012", "2013", "2014"))
+tabela_uvoz_plastike_in_gume <- melt(tabela_uvoz_plastike_in_gume, variable.name = "Leto",
+                            value.name = "Platika in guma", na.rm = TRUE)
+tabela_uvoz_plastike_in_gume$Leto <- tabela_uvoz_plastike_in_gume$Leto %>% as.character() %>%
+  strapplyc("([0-9]+)")
+tabela_uvoz_plastike_in_gume$Leto <- tabela_uvoz_plastike_in_gume$Leto %>% as.numeric()
+
 
 tabela_uvoz_lesa <- read.csv2("podatki/import_wood.csv", skip=1, na.strings = ";", stringsAsFactors = FALSE,
                                         fileEncoding = "Windows-1250", col.names = c("Država","2010", "2011", "2012", "2013", "2014"))
+tabela_uvoz_lesa <- melt(tabela_uvoz_lesa, variable.name = "Leto",
+                            value.name = "Les", na.rm = TRUE)
+tabela_uvoz_lesa$Leto <- tabela_uvoz_lesa$Leto %>% as.character() %>%
+  strapplyc("([0-9]+)")
+tabela_uvoz_lesa$Leto <- tabela_uvoz_lesa$Leto %>% as.numeric()
+
 
 
 # Izvoz ZDA:
 
 tabela_izvoz_vseh_produktov <- read.csv2("podatki/export_all_product.csv", skip=1, na.strings = ";", stringsAsFactors = FALSE,
                                         fileEncoding = "Windows-1250", col.names = c("Država","2010", "2011", "2012", "2013", "2014"))
+tabela_izvoz_vseh_produktov <- melt(tabela_izvoz_vseh_produktov, variable.name = "Leto",
+                               value.name = "Vsi produkti", na.rm = TRUE)
+tabela_izvoz_vseh_produktov$Leto <- tabela_izvoz_vseh_produktov$Leto %>% as.character() %>%
+  strapplyc("([0-9]+)")
+tabela_izvoz_vseh_produktov$Leto <- tabela_izvoz_vseh_produktov$Leto %>% as.numeric()
+
 
 tabela_izvoz_zivali <- read.csv2("podatki/export_animal.csv", skip=1, na.strings = ";", stringsAsFactors = FALSE,
                                 fileEncoding = "Windows-1250", col.names = c("Država","2010", "2011", "2012", "2013", "2014"))
+tabela_izvoz_zivali <- melt(tabela_izvoz_zivali, variable.name = "Leto",
+                            value.name = "Živali", na.rm = TRUE)
+tabela_izvoz_zivali$Leto <- tabela_izvoz_zivali$Leto %>% as.character() %>%
+  strapplyc("([0-9]+)")
+tabela_izvoz_zivali$Leto <- tabela_izvoz_zivali$Leto %>% as.numeric()
+
 
 tabela_izvoz_zelenjave <- read.csv2("podatki/export_vegetable.csv", skip=1, na.strings = ";", stringsAsFactors = FALSE,
                                    fileEncoding = "Windows-1250", col.names = c("Država","2010", "2011", "2012", "2013", "2014"))
+tabela_izvoz_zelenjave <- melt(tabela_izvoz_zelenjave, variable.name = "Leto",
+                               value.name = "Zelenjava", na.rm = TRUE)
+tabela_izvoz_zelenjave$Leto <- tabela_izvoz_zelenjave$Leto %>% as.character() %>%
+  strapplyc("([0-9]+)")
+tabela_izvoz_zelenjave$Leto <- tabela_izvoz_zelenjave$Leto %>% as.numeric()
+
 
 tabela_izvoz_mineralov <- read.csv2("podatki/export_minerals.csv", skip=1, na.strings = ";", stringsAsFactors = FALSE,
                                    fileEncoding = "Windows-1250", col.names = c("Država","2010", "2011", "2012", "2013", "2014"))
+tabela_izvoz_mineralov <- melt(tabela_izvoz_mineralov, variable.name = "Leto",
+                            value.name = "Minerali", na.rm = TRUE)
+tabela_izvoz_mineralov$Leto <- tabela_izvoz_mineralov$Leto %>% as.character() %>%
+  strapplyc("([0-9]+)")
+tabela_izvoz_mineralov$Leto <- tabela_izvoz_mineralov$Leto %>% as.numeric()
+
 
 tabela_izvoz_goriva <- read.csv2("podatki/export_fuels.csv", skip=1, na.strings = ";", stringsAsFactors = FALSE,
                                 fileEncoding = "Windows-1250", col.names = c("Država","2010", "2011", "2012", "2013", "2014"))
+tabela_izvoz_goriva <- melt(tabela_izvoz_goriva, variable.name = "Leto",
+                            value.name = "Gorivo", na.rm = TRUE)
+tabela_izvoz_goriva$Leto <- tabela_izvoz_goriva$Leto %>% as.character() %>%
+  strapplyc("([0-9]+)")
+tabela_izvoz_goriva$Leto <- tabela_izvoz_goriva$Leto %>% as.numeric()
+
 
 tabela_izvoz_kemikalij <- read.csv2("podatki/export_chemicals.csv", skip=1, na.strings = ";", stringsAsFactors = FALSE,
                                    fileEncoding = "Windows-1250", col.names = c("Država","2010", "2011", "2012", "2013", "2014"))
+tabela_izvoz_kemikalij <- melt(tabela_izvoz_kemikalij, variable.name = "Leto",
+                            value.name = "Kemikalije", na.rm = TRUE)
+tabela_izvoz_kemikalij$Leto <- tabela_izvoz_kemikalij$Leto %>% as.character() %>%
+  strapplyc("([0-9]+)")
+tabela_izvoz_kemikalij$Leto <- tabela_izvoz_kemikalij$Leto %>% as.numeric()
+
 
 tabela_izvoz_plastike_in_gume <- read.csv2("podatki/export_plastic_and_rubber.csv", skip=1, na.strings = ";", stringsAsFactors = FALSE,
                                           fileEncoding = "Windows-1250", col.names = c("Država","2010", "2011", "2012", "2013", "2014"))
+tabela_izvoz_plastike_in_gume <- melt(tabela_izvoz_plastike_in_gume, variable.name = "Leto",
+                            value.name = "Plastika in guma", na.rm = TRUE)
+tabela_izvoz_plastike_in_gume$Leto <- tabela_izvoz_plastike_in_gume$Leto %>% as.character() %>%
+  strapplyc("([0-9]+)")
+tabela_izvoz_plastike_in_gume$Leto <- tabela_izvoz_plastike_in_gume$Leto %>% as.numeric()
+
 
 tabela_izvoz_lesa <- read.csv2("podatki/export_wood.csv", skip=1, na.strings = ";", stringsAsFactors = FALSE,
                               fileEncoding = "Windows-1250", col.names = c("Država","2010", "2011", "2012", "2013", "2014"))
-
+tabela_izvoz_lesa <- melt(tabela_izvoz_lesa, variable.name = "Leto",
+                            value.name = "Les", na.rm = TRUE)
+tabela_izvoz_lesa$Leto <- tabela_izvoz_lesa$Leto %>% as.character() %>%
+  strapplyc("([0-9]+)")
+tabela_izvoz_lesa$Leto <- tabela_izvoz_lesa$Leto %>% as.numeric()
 
 
 # PODATKI V OBLIKI xml :
@@ -62,58 +156,56 @@ library(rvest)
 library(dplyr)
 library(gsubfn)
 
-# uvoz tabele držav, za katere je ZDA glavni partner v izvozu v % :
+# uvoz tabele držav, za katere je ZDA glavni partner v izvozu 2014 v % :
 
 link1 <- "http://en.wikipedia.org/wiki/List_of_the_largest_trading_partners_of_the_United_States"
 stran1 <- html_session(link1) %>% read_html()
-tabela_partnerstvo_izvoz <- stran %>% html_nodes(xpath="//table[@class='wikitable 
-                               sortable']") %>% .[[1]] %>% html_table()
+tabela_partnerstvo_izvoz <- stran1 %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>% .[[1]] %>% html_table()
+tabela_partnerstvo_izvoz <- tabela_partnerstvo_izvoz %>%
+  rename(Država = Region, Procent = Percentage)
+tabela_partnerstvo_izvoz$Procent <- tabela_partnerstvo_izvoz$Procent %>%
+  strapplyc("([0-9.]+)") %>% as.numeric()
+tabela_partnerstvo_izvoz$Država <- tabela_partnerstvo_izvoz$Država %>%
+  strapplyc("([a-zA-Z ]+)")
 
 
-# uvoz tabele držav, za katere je ZDA glavni partner v uvozu v % :
-
+# uvoz tabele držav, za katere je ZDA glavni partner v uvozu 2014 v % :
 link2 <- "http://en.wikipedia.org/wiki/List_of_the_largest_trading_partners_of_the_United_States"
 stran2 <- html_session(link2) %>% read_html()
-tabela_partnerstvo_uvoz <- stran %>% html_nodes(xpath="//table[@class='wikitable 
-                                                sortable']") %>% .[[2]] %>% html_table()
+tabela_partnerstvo_uvoz <- stran2 %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>% .[[2]] %>% html_table()
+tabela_partnerstvo_uvoz <- tabela_partnerstvo_uvoz %>%
+  rename(Država = Region, Procent = Percentage)
+tabela_partnerstvo_uvoz$Procent <- tabela_partnerstvo_uvoz$Procent %>%
+  strapplyc("([0-9.]+)") %>% as.numeric()
+tabela_partnerstvo_uvoz$Država <- tabela_partnerstvo_uvoz$Država %>%
+  strapplyc("([a-zA-Z ]+)")
 
 
+# GRAFI :
 
-
-# Grafi :
-# celoten uvoz ZDA
+# Uvoz dvajsetih največjih uvoznic ZDA v letih 2010 do 2014
 
 Države<-tabela_uvoz_vseh_produktov$Država
-uvoz_v_tisoč_USD_v_letu_2010<-tabela_uvoz_vseh_produktov$X2010
-uvoz_v_tisoč_USD_v_letu_2011<-tabela_uvoz_vseh_produktov$X2011
-uvoz_v_tisoč_USD_v_letu_2012<-tabela_uvoz_vseh_produktov$X2012
-uvoz_v_tisoč_USD_v_letu_2013<-tabela_uvoz_vseh_produktov$X2013
-uvoz_v_tisoč_USD_v_letu_2014<-tabela_uvoz_vseh_produktov$X2014
 
 uvoz_v_celoti <- ggplot(tabela_uvoz_vseh_produktov)+ 
-    aes(x=Države, y=c("uvoz_v_tisoč_USD_v_letu_2010","uvoz_v_tisoč_USD_v_letu_2011",
-    "uvoz_v_tisoč_USD_v_letu_2012","uvoz_v_tisoč_USD_v_letu_2013","uvoz_v_tisoč_USD_v_letu_2014"))+
+    aes(x=Države, y=Leto)+
     geom_line(colour = "red")+
-    ggtitle("Skupni uvoz ZDA v letih 2010 do 2014")+
+    ggtitle("Uvoz dvajsetih največjih uvoznic ZDA v letih 2010 do 2014")+
     theme(plot.title = element_text(lineheight=.8, face="bold"))
 
 uvoz_v_celoti
 
 
-# celoten izvoz ZDA
+# Izvoz dvajsetih največjih izvoznic ZDA v letih 2010 do 2014
 
 Države<-tabela_izvoz_vseh_produktov$Država
-izvoz_v_tisoč_USD_v_letu_2010<-tabela_izvoz_vseh_produktov$X2010
-izvoz_v_tisoč_USD_v_letu_2011<-tabela_izvoz_vseh_produktov$X2011
-izvoz_v_tisoč_USD_v_letu_2012<-tabela_izvoz_vseh_produktov$X2012
-izvoz_v_tisoč_USD_v_letu_2013<-tabela_izvoz_vseh_produktov$X2013
-izvoz_v_tisoč_USD_v_letu_2014<-tabela_izvoz_vseh_produktov$X2014
 
-izvoz_v_celoti<-ggplot(tabela_izvoz_vseh_produktov)+
-  aes(x=Države, y=c("izvoz_v_tisoč_USD_v_letu_2010","izvoz_v_tisoč_USD_v_letu_2011",
-  "izvoz_v_tisoč_USD_v_letu_2012","izvoz_v_tisoč_USD_v_letu_2013","izvoz_v_tisoč_USD_v_letu_2014"))+
-  geom_line(colour = "red")+
-  ggtitle("Skupni izvoz ZDA v letih 2010 do 2014")+
+izvoz_v_celoti<-ggplot(tabela_izvoz_vseh_produktov %>%
+                         filter(Leto == 2010) %>%
+                         arrange(desc(`Vsi produkti`)) %>% head(10))+
+  aes(x=Država, y=`Vsi produkti`)+
+  geom_bar(stat = "identity", fill = "green")+
+  ggtitle("Izvoz dvajsetih največjih izvoznic ZDA v letih 2010 do 2014")+
   theme(plot.title = element_text(lineheight=.8, face="bold"))
 
 izvoz_v_celoti
